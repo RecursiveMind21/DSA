34847cc9b2b77eb8eddba090b07c223ab06f3ed1
“Dinesh <dbodhe03@gmail.com>
2024-02-10 00:16:18 +0530

    Constructors

--- OOPS/ClassConstructors.cpp
+++ OOPS/ClassConstructors.cpp
@@ -1,5 +1,6 @@
 //program with constructor in cpp
 #include<iostream>          //header file
+#include<string.h>
 
 using namespace std;            
 
@@ -7,9 +8,9 @@ class Hero                 //class declaration
 {
     int health;            
     char level;
+    
 
     public:
-
     //this is called as default constructor
     Hero(){
         cout << "Default Constructor Called";
@@ -26,11 +27,16 @@ class Hero                 //class declaration
         this->health = health;
         this->level = level;
     }
-    
     void Display()                      //function 
     {
         cout << "Health: " << health << endl;
         cout << "Level: " << level << endl;
+    }
+    void setHealth(int h){              //setter to set health
+        health = h;
+    }
+    void setLevel(int l){               //setter to ser level
+        level = l;
     }
 };
 
@@ -38,14 +44,19 @@ int main()
 {
     Hero h1;                //creating object
 
+    cout << "                   \e[7mPARAMETERISED CONSTRUCTOR\e[0m              " << endl;
+    cout << "-------------------------------------------------------------------" << endl;
+
+    cout << "Display of object with parameterised constructor." << endl;
     Hero h2(70);            //creating object with parameterised constructor
     h2.Display();           //for display
 
-    cout << endl;
+    cout << "-------------------------------------------------------------------" << endl;
+
+    cout << "Display object with parameterised constructor with multiple parameter" << endl;
     Hero h3(80,'D');        //creating object with parameterised constructor
     h3.Display();           //with multiple parameter
 
-    cout << endl;
     cout << endl;
     cout << endl;
 
